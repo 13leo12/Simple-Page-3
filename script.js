17348777e3c341ef285ffb7b20ca6b5f7eb5a96e
Dashtamirov <98391075+13leo12@users.noreply.github.com>
2022-10-26 17:23:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+        effect: "cards",
+        grabCursor: true,
+        navigation: {
+            nextEl: '.costumer_swiper_button_next',
+            prevEl: '.costumer_swiper_button_prev',
+        },
+    });
